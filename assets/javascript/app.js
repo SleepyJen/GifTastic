6749a568b57282e6cef700811a1a47998fbc0d0a
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-24 13:51:50 -0700

    added functionality to buttons and gifs to screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     var topics = ["happy", "angry", "sad", "frustrated", "sneaky", "scared", "surprised", "shy", "bossy", "sleepy",
         "embarrassed", "mean", "proud", "nervous", "silly", "disappointed", "confused", "impatient", "curious", "kind",
         "annoyed", "obnoxious", "excited", "grumpy"];
-    var url = "https://api.giphy.com/v1/gifs/search?api_key=VgeYY3oYCCgPBIDvALitcLV5YJDro9CD&q=disney&limit=10&offset=0&rating=G&lang=en";
 
     for (let i = 0; i < topics.length; i++) {
         let btn = $('<button>');
@@ -10,4 +9,33 @@ $(document).ready(function () {
         btn.text(topics[i]);
         $('.buttons').append(btn);
     }
+
+    $(document).on('click', 'button', function () {
+        let attribute = $(this).attr('data');
+        var url = `https://api.giphy.com/v1/gifs/search?api_key=VgeYY3oYCCgPBIDvALitcLV5YJDro9CD&q=${attribute}&limit=10&offset=0&rating=G&lang=en`;
+        $.ajax({
+            method: 'GET',
+            url: url
+        }).then(result => {
+            console.log(result);
+            let data = result.data;
+
+            for (let i = 0; i < data.length; i++) {
+                let gifDiv = $('<div>');
+                let rating = $('<p>');
+                let gifs = $('<img>');
+
+                gifs.attr('src', data[i].images.fixed_height.url);
+                gifs.attr('class', 'images');
+                gifDiv.attr('class', 'gifBoxes');
+                rating.text("Rating: " + data[i].rating);
+
+                gifDiv.append(rating);
+                gifDiv.append(gifs);
+                $('#gifs').prepend(gifDiv);
+            }
+
+        });
+    });
+
 });
