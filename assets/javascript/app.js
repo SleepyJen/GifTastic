c5f85444a8633a1026c24f6bd9b2bb1f69f40652
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-24 12:59:58 -0700

    added buttons for topics and set up divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
-    console.log("Hello World");
+    var topics = ["happy", "angry", "sad", "frustrated", "sneaky", "scared", "surprised", "shy", "bossy", "sleepy",
+        "embarrassed", "mean", "proud", "nervous", "silly", "disappointed", "confused", "impatient", "curious", "kind",
+        "annoyed", "obnoxious", "excited", "grumpy"];
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=VgeYY3oYCCgPBIDvALitcLV5YJDro9CD&q=disney&limit=10&offset=0&rating=G&lang=en";
 
+    for (let i = 0; i < topics.length; i++) {
+        let btn = $('<button>');
+        btn.attr({ 'class': 'btn', 'data': topics[i] });
+        btn.text(topics[i]);
+        $('.buttons').append(btn);
+    }
 });
